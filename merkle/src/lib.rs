bcbe2742f884309ae8493b1dc322d138dd438e41
guoxbin <guoxbin@gmail.com>
2020-06-22 16:13:26 +0800

    add vec

--- merkle/src/lib.rs
+++ merkle/src/lib.rs
@@ -156,6 +156,7 @@
 #![cfg_attr(feature="nightly", allow(unstable_features))]
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), feature(alloc))]
 
 /// Hash infrastructure for items in Merkle tree.
 pub mod hash;
